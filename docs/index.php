c4e9284475879881ea037dc8f63e3b7673bed432
Zacronix <j_halff@live.nl>
2017-10-11 11:32:24 +0200

    added some files

---
+++ docs/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="theme.css">
+    <title>Education Storage</title>
+  </head>
+  <body>
+
+  </body>
+</html>
